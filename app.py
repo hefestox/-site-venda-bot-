55a2b072e4f2c4ebdbc9ef357a05374d443df05b
hefestox <pr.luizricardo@gmail.com>
2026-04-06 11:14:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-with open("index.html", "r", encoding="utf-8") as f:
-    html_content = f.read()
+with open("index_whatsapp_editado.html", "r", encoding="utf-8") as f:
+    html = f.read()
 
-components.html(html_content, height=1200, scrolling=True)
+components.html(html, height=1200, scrolling=True)
